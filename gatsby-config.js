046eafd6c076318d08975b4a2c1ffb77bde70811
Emiel van Rijn <mail@emielvanrijn.nl>
2018-01-29 21:55:43 +0100

    Add Contentful as data-source through plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      },
+    },
   ],
 };
