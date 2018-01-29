f211d2dde51eebcc6a5e81b1c8d1e1ca275dbac8
Emiel van Rijn <mail@emielvanrijn.nl>
2018-01-29 21:15:11 +0100

    Add VacatureIndex

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 const HomePage = () => (
   <div>
     <h1>S.V. Sticky</h1>
+    <Link to="/vacatures/">VacatureIndex</Link>
   </div>
 )
 
---
+++ src/pages/vacatures.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+export default VacatureIndex => {
+    return (
+        <h1>Vacaturessss</h1>
+    );
+}
