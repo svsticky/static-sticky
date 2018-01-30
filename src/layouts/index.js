369fa3266d8436bae8c2030b340f4b4a5c766da4
Emiel van Rijn <mail@emielvanrijn.nl>
2018-01-30 23:24:04 +0100

    Add navigation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import Link from 'gatsby-link';
+import { Container, Menu } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
 import '../css/index.scss';
@@ -7,10 +8,16 @@ import '../css/index.scss';
 
 export default ({ children }) => (
     <div>
-        NAVBAR
+        <Menu size='large' fixed='top'>
+            <Link to='/'>
+                <Menu.Item name='Home'/>
+            </Link>
+            <Link to='/vacatures'>
+                <Menu.Item name='Vacatures'/>
+            </Link>
+        </Menu>
         <Container>
             {children()}
         </Container>
-        FOOTER
     </div>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,7 @@ import Link from 'gatsby-link';
 import { Button, Container } from 'semantic-ui-react'
 
 const HomePage = () => (
-  <Container>
-    <h1>S.V. Sticky</h1>
-    <Link to="/vacatures/">VacatureIndex</Link>
-  </Container>
+    <h1>Homepage</h1>
 )
 
 export default HomePage
