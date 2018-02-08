0843a23091fcfb1c946c175271127a9b0944af55
Emiel van Rijn <mail@emielvanrijn.nl>
2018-02-08 19:18:33 +0100

    Use Radium as overrider for semantic-ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import Link from 'gatsby-link';
 import { Container, Menu } from 'semantic-ui-react';
-
+import Radium from 'radium';
+import Link from 'gatsby-link';
 import logo from '../images/logo-sticky.png';
 
+const style = {
+  navbar: {
+    backgroundColor: '#000078',
+    color: 'white',
+  },
+  navbaritem: {
+    color: 'white',
+  },
+  logo: {
+    height: '3.2em',
+    marginTop: '0.3em',
+  },
+};
+
 const Navbar = () => (
-  <Menu fixed="top">
-    <Container>
-      <Menu inverted secondary>
-        <Link to="/"><img src={logo} alt="" /></Link>
-        <Menu.Item as={Link} to="/bestuur">Bestuur</Menu.Item>
-        <Menu.Item as={Link} to="/vacatures" content="Vacatures" />
-        <Menu.Item as={Link} to="/onderwijs" content="Onderwijs" />
-      </Menu>
-    </Container>
-  </Menu>
+  <div>
+    <Menu style={style.navbar} fixed="top">
+      <Container>
+        <Menu secondary>
+          <Link to="/"><img src={logo} alt="" style={style.logo} /></Link>
+          <Menu.Item as={Link} to="/bestuur" content="Bestuur" style={style.navbaritem} />
+          <Menu.Item as={Link} to="/vacatures" content="Vacatures" style={style.navbaritem} />
+          <Menu.Item as={Link} to="/onderwijs" content="Onderwijs" style={style.navbaritem} />
+        </Menu>
+      </Container>
+    </Menu>
+  </div>
 );
 
-export default Navbar;
+export default Radium(Navbar);
