cfc746f392ab12fa28746ed4cd50153d62319e93
Glenn Stewart <glennstewart95@hotmail.com>
2018-02-28 15:25:43 +0100

    added partners page

---
+++ src/pages/partners.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Partner from '../components/Partner';
+import { Card } from 'semantic-ui-react';
+
+
+const PartnerIndexPage =  ({data}) => {
+  const partners = data.allContentfulPartner.edges;
+
+  return(
+    <div>
+      {partners.map(partner => <Partner key={partner.node.id} partner={partner.node}/>)}
+    </div>
+  );
+};
+
+
+export const PartnerListQuery = graphql`
+  query PartnerListQuery {
+    allContentfulPartner {
+      edges {
+        node {
+          id
+          logo {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default PartnerIndexPage
