cceeb7ee930d4312209f70a3ba6223a32338a718
Emiel van Rijn <mail@emielvanrijn.nl>
2018-03-02 11:53:57 +0100

    Add SubMenu to all MenuItems in MobileNavbar

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Icon, Menu } from 'semantic-ui-react';
 import Link from 'gatsby-link';
 import Radium from 'radium';
+import MobileSubMenu from '../components/MobileSubMenu';
 import globalStyles from '../styles/globalStyles';
 import responsive from '../styles/responsive';
 
@@ -15,22 +16,49 @@ const styles = {
   },
 };
 
-const MobileNavbar = () =>
-  <div style={responsive.hide.tabletUp}>
-    <Menu widths={3} fixed='bottom' icon='labeled' secondary style={styles.navbar} size='mini'>
-      <Menu.Item as={Link} to="/bestuur"style={styles.navbaritem}>
-        <Icon name='users'/>
-        Bestuur
-      </Menu.Item>
-      <Menu.Item as={Link} to="/carriere" style={styles.navbaritem}>
-        <Icon name='briefcase'/>
-        Carrière
-      </Menu.Item>
-      <Menu.Item as={Link} to="/onderwijs" style={styles.navbaritem}>
-        <Icon name='student'/>
-        Onderwijs
-      </Menu.Item>
-    </Menu>
-  </div>
+class MobileNavbar extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      showCareerMenu: false,
+      showEducationMenu: false,
+      activeItem: null,
+    }
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  handleItemClick = (e, { name }) => {
+    if(this.state.activeItem === name) {
+      console.log(name + ' already the activeItem');
+      this.setState({
+        activeItem: null
+      })
+    } else {
+    this.setState({
+      activeItem: name,
+    })};
+  }
+
+  render() {
+    return (
+    <div style={responsive.hide.tabletUp}>
+      <MobileSubMenu activeItem={this.state.activeItem}/>
+      <Menu widths={3} fixed='bottom' icon='labeled' secondary style={styles.navbar} size='mini'>
+        <Menu.Item name="association" style={styles.navbaritem} onClick={this.handleItemClick}>
+          <Icon name='users'/>
+          Vereniging
+        </Menu.Item>
+        <Menu.Item name="career" style={styles.navbaritem} onClick={this.handleItemClick}>
+          <Icon name='briefcase'/>
+          Carrière
+        </Menu.Item>
+        <Menu.Item name="education" style={styles.navbaritem} onClick={this.handleItemClick}>
+          <Icon name='student'/>
+          Onderwijs
+        </Menu.Item>
+      </Menu>
+    </div>
+  )};
+  }
 export default Radium(MobileNavbar);
---
+++ src/components/MobileSubMenu.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Icon, Menu, Segment } from 'semantic-ui-react';
+
+const styles = {
+  mobileSubMenuPopUp: {
+    position: 'fixed',
+    bottom: '3.8em',
+    width: '100%',
+    textAlign: 'center',
+    padding: '0',
+    margin: '0'
+  },
+  mobileSubMenu: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-end'
+  },
+  mobileSubMenuItem: {
+    width: '33%',
+    alignSelf: 'flex-start',
+    height: '4em',
+    border: '1px solid #efefef',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '0.15%'
+  }
+}
+
+const subMenuItems = {
+  association: [
+    'Besturen'
+  ],
+  career: [
+    'Vacatures',
+    'Partners',
+    'Stages/Scripties'
+  ],
+  education: [
+    'Links'
+  ]
+}
+
+const MobileSubMenu = (props) => {
+  return(
+    props.activeItem ? (
+      <Segment style={styles.mobileSubMenuPopUp}>
+        <div style={styles.mobileSubMenu}>
+          {subMenuItems[props.activeItem].map(item =>
+            <div style={styles.mobileSubMenuItem}>
+              {item}
+            </div>)
+          }
+        </div>
+      </Segment>
+    ) : null
+  );
+};
+
+export default MobileSubMenu;
