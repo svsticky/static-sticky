6a5e0f303f8402ce5dd88f304482e97ecdc02fec
Emiel van Rijn <mail@emielvanrijn.nl>
2018-05-08 19:13:13 +0200

    Introduce Card-atom

---
+++ src/atoms/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border-radius: 8px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+`
+
+export default Card;
