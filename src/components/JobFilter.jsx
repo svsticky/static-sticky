a270939f9b8b430aedb301ac80ac74353bcb3fca
Niels <niels.kwadijk@gmail.com>
2018-10-05 15:51:36 +0200

    Made progress on adding the filter options in vacatures. The dropdown menu's are easy to get working. Got stuck on calling the update methods to actually filter.

--- src/components/JobFilter.jsx
+++ src/components/JobFilter.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Card } from 'semantic-ui-react';
+import { Card, Dropdown } from 'semantic-ui-react';
 
 
 const studieFilterOptions = [
-  'Informatica',
-  'Informatiekunde',
-  'Gametechnologie',
-  'Artificial Intelligence',
-  'Business Informatics',
-  'Computing Science',
-  'Game and Media Technology',
+  {key: 1, text: 'Informatica', value: 'Informatica'},
+  {key: 2, text: 'Informatiekunde', value: 'Informatica'},
+  {key: 3, text: 'Gametechnologie', value: 'Informatica'},
+  {key: 4, text: 'Artificial Intelligence', value: 'Artificial Intelligence'},
+  {key: 5, text: 'Business Informatics', value: 'Business Informatics'},
+  {key: 6, text: 'Computing Science', value: 'Computing Science'},
+  {key: 7, text: 'Game and Media Technology', value: 'Game and Media Technology'}
 ];
 
 const typeFilterOptions = [
@@ -21,8 +21,7 @@ const typeFilterOptions = [
   'Stage',
   'Traineeship',
 ];
-
-
+//The problem lies in the first dropdown -> onChange. Somehow it won't allow to filter
 const JobFilter = props => (
   <JobFilterWrapper>
       <div>
@@ -30,40 +29,34 @@ const JobFilter = props => (
           <span>{props.studiesFilter.length} studie(s) geselecteerd:</span> :
           <span>Selecteer op studie(s)</span> }
       </div>
-      <div
-        multiple
-        value={props.studiesFilter}
-        onChange={e => (props.updateStudiesFilter(e.target.value))}
-        input={<div id="select-study" />}
-      >
-        { studieFilterOptions.map(option => (
-          <div
-            key={option}
-            value={option}
-          >
-            {option}
-          </div>
-        ))}
-      </div>
+      <Dropdown
+          search multiple selection
+          options={studieFilterOptions}
+          placeholder="Filter op studie"
+          onChange= {(e, {value}) => props.updateStudiesFilter(value)}/>
+
       <div htmlFor="select-type" className="input-label">
         { props.typesFilter.length > 0 ?
           <span>{props.typesFilter.length} type(n) geselecteerd:</span> :
           <span>Selecteer op type(n)</span> }
       </div>
       <div
-        multiple
         value={props.typesFilter}
-        onChange={e => (props.updateTypesFilter(e.target.value))}
         input={<div id="select-type" />}
       >
+      <Dropdown item text="Selecteer op type" direction="Left" selection
+                onChange={e => (props.updateTypesFilter(e.target.value))}>
+        <Dropdown.Menu>
         { typeFilterOptions.map(option => (
-          <div
-            key={option}
-            value={option}
-          >
-            {option}
-          </div>
+          <Dropdown.Item
+          className="item"
+          key={option}
+          value ={option}
+          > <p className="item-text">{option}</p>
+          </Dropdown.Item>
         ))}
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
   </JobFilterWrapper>
 );
