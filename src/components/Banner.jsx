33a5cc3904c950921b6356b933de28b744542985
Niels <niels.kwadijk@gmail.com>
2018-10-13 17:42:13 +0200

    Add utils.jsx for global functions, add css conventions

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { graphql, StaticQuery } from 'gatsby';
 import { Card, Image } from 'semantic-ui-react';
+import { getRandom } from '../utils.jsx';
 
 class Banner extends React.Component {
 
   renderLogos = allLogos => (
     <Card fluid >
-      <Card.Content className="cardcontent">
+      <Card.Content className="card-content">
         {getRandom(allLogos, 5).map(logo => {
           return(
             <Image className="image" key={logo.node.id} size="small" src={logo.node.image.file.url} />
@@ -26,25 +27,9 @@ class Banner extends React.Component {
   }
 }
 
-const getRandom = (arr, n) => {
-  const result = new Array(n);
-  let len = arr.length;
-  const taken = new Array(len);
-
-  if(n > len)
-    return arr;
-
-  while(n--){
-    const x = Math.floor(Math.random() * len);
-    result[n] = arr[x in taken ? taken[x] : x];
-    taken[x] = --len in taken ? taken[len] : len;
-  }
-  return result;
-}
-
 const BannerWrapper = styled.div`
   padding-bottom: 3em;
-  .cardcontent{
+  .card-content{
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
---
+++ src/utils.jsx
@@ -0,0 +1,17 @@
+//Place here functions that can be used in different components
+
+export const getRandom = (arr, n) => {
+  const result = new Array(n);
+  let len = arr.length;
+  const taken = new Array(len);
+
+  if(n > len)
+    return arr;
+
+  while(n--){
+    const x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
