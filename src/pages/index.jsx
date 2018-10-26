a61332e5cb705cb3efd7cd712c4bc96a4d4090b7
Niels <niels.kwadijk@gmail.com>
2018-10-26 10:23:53 +0200

    Create grid for index-page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
 import Layout from '../components/Layout'
 import Banner from '../components/Banner'
 import News from '../components/News'
 
-export default () => (
-  <>
+const Index = (props) => {
+
+  return(
     <Layout>
-      <h1>Homepage</h1>
-      <Banner />
-      <News />
+      <IndexWrapper>
+        <h1>Homepage</h1>
+        <div className="container">
+          <div className="banner">
+            <Banner />
+          </div>
+          <div className="news">
+            <News />
+          </div>
+        </div>
+      </IndexWrapper>
     </Layout>
-  </>
-)
+  )
+}
+
+export const IndexWrapper = styled.div`
+  &&&
+
+  .container {
+    display: grid;
+    grid-template-columns: 33% 33% 33%;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "banner banner banner"
+      "news news widget"
+      "awidget awidget awidget";
+  }
+  .banner {
+    grid-area: banner;
+  }
+  .news {
+    grid-area: news;
+  }
+`
+
+export default Index
