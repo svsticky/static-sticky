04f497e9b2fc0996e82f969905395f68fc20ddb7
Colin de Roos <colinderoos@gmail.com>
2018-12-07 12:08:58 +0100

    Clean up activity info layout and css

--- src/components/activities/Activity.jsx
+++ src/components/activities/Activity.jsx
@@ -20,20 +20,17 @@ export default class Activity extends React.Component {
   render() {
     const { poster, name } = this.props.activity;
     return (
-      <ActivityWrapper
+      <div
         onMouseEnter={this.flipCard}
         onMouseLeave={this.flipCard}
         onTouchEnd={this.flipCard}
       >
         <ReactCardFlip isFlipped={this.state.isFlipped}>
           <div key="front">{renderPoster(poster, name)}</div>
-          <div key="back">{renderInfo(this.props)}</div>
+          <div key="back">{renderInfo(this.props.activity)}</div>
         </ReactCardFlip>
-        {/* Render the image again for sizing (position: absolute on the ReactCardFlip removes it from the document flow)
-            Also see: https://github.com/AaronCCWong/react-card-flip/issues/10
-            and https://stackoverflow.com/a/46281339*/}
         <Hidden>{renderPoster(poster, name)}</Hidden>
-      </ActivityWrapper>
+      </div>
     );
   }
 }
@@ -42,59 +39,41 @@ const renderPoster = (posterUrl, activityName) => (
   <Image size="medium" src={posterUrl} alt={'Poster voor ' + activityName} />
 );
 
-const renderInfo = props => {
-  const { id, location, name, price } = props.activity;
-  return (
-    <Card fluid className={'info'}>
-      <div className="content">
-        <h3>{name}</h3>
-        {location ? (
+const renderInfo = ({ id, location, name, price }) =>
+  (
+    <Card>
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+      </Card.Content>
+      <Card.Content>
+        <Card.Description>
+          {location && (
+            <p>
+              <strong>Locatie: </strong>
+              <em>{location}</em>
+            </p>
+          )}
           <p>
-            <strong>Locatie: </strong>
-            <em>{location}</em>
+            <strong>Prijs: </strong>
+            <em>
+              {price !== 0
+                ? '€' + (price.split('.')[1].length === 2 ? price : price + '0')
+                : 'Gratis!'}
+            </em>
           </p>
-        ) : null}
-        <p>
-          <strong>Prijs: </strong>
-          <em>
-            {price !== 0
-              ? '€' + (price.split('.')[1].length === 2 ? price : price + '0')
-              : 'Gratis!'}
-          </em>
-        </p>
-      </div>
+        </Card.Description>
+      </Card.Content>
       <Button
         href={'https://koala.svsticky.nl/activities/' + id}
         target="_blank"
-      >
-        <p>
-          Inschrijven <i className="item-text icon external" />
-        </p>
-      </Button>
+        content="Inschrijven"
+        icon="external"
+        labelPosition="right"
+      />
     </Card>
   );
-};
 
 const Hidden = styled.div`
   z-index: -1;
   visibility: hidden;
-`;
-
-const ActivityWrapper = styled.div`
-  .info {
-    margin: 0 !important;
-    display: flex !important;
-    flex-direction: column;
-    .content {
-      flex: 1;
-      padding: 1em !important;
-      h3 {
-        border-bottom: 1px solid #ddd;
-        padding-bottom: 0.5em;
-      }
-    }
-  }
-  .icon {
-    margin-left: 0.4em;
-  }
 `;
