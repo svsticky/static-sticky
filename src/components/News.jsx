83e5fd6614e86d7ec985177eb67ecd986ed89612
Phavion <niels.kwadijk@gmail.com>
2018-12-12 11:25:18 +0100

    add pager

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 import { graphql, StaticQuery } from 'gatsby';
+import Pager from 'react-pager';
 
 class News extends React.Component {
-  renderNewsItems = allItems => (
+  constructor(props){
+    super(props);
+
+    this.handlePageChanged = this.handlePageChanged.bind(this);
+    this.newsItems = this.props.data.allContentfulNewsArticles.edges;
+
+    this.state = {
+      total: Math.ceil(this.newsItems.length / 5),
+      current: 0,
+      visiblePage: 3,
+    };
+  }
+
+  handlePageChanged(newPage) {
+    this.setState({ current: newPage });
+  }
+
+  renderNewsItems = (allItems, pageNum) => (
     <div>
-      {allItems.map(item => {
+      {allItems.filter(fitem => allItems.indexOf(fitem) >= pageNum * 5 &&
+        allItems.indexOf(fitem) < pageNum * 5 + 5).map(item => {
         return (
           <div key={item.node.id}>
             <h3>
@@ -26,7 +45,14 @@ class News extends React.Component {
   render() {
     return (
       <NewsWrapper>
-        {this.renderNewsItems(this.props.data.allContentfulNewsArticles.edges)}
+        {this.renderNewsItems(this.newsItems, this.state.current)}
+          <Pager
+            total={this.state.total}
+            current={this.state.current}
+            visiblePages={this.state.visiblePage}
+            titles={{ first: 'First', last: 'Last'}}
+            onPageChanged={this.handlePageChanged}
+            />
       </NewsWrapper>
     );
   }
@@ -36,6 +62,57 @@ export const NewsWrapper = styled.div`
   .content {
     margin-bottom: 1em;
     border-bottom: 1px solid #ddd;
+  }
+  .pagination {
+    font-size: 25px;
+    display: inline-block;
+    padding-left: 0;
+    margin: 10px 0;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+
+    > li {
+      display: inline;
+      > a,
+      > span {
+        position: relative;
+        float: left;
+        border-left: 1px solid #ddd;
+      }
+      &:first-child {
+        > a,
+        > span {
+          margin-left: 0;
+        }
+      }
+    }
+    > li > a,
+    > li > span {
+      &:hover,
+      &:focus {
+        background-color: #ddd;
+      }
+    }
+    > .active > a,
+    > .active > span {
+      &,
+      &:hover,
+      &:focus {
+        z-index: 2;
+        color: #4183c4;
+        cursor: default;
+      }
+    }
+    > .disabled {
+      > span,
+      > span:hover,
+      > span:focus,
+      > a,
+      > a:hover,
+      > a:focus {
+        color: #444;
+      }
+    }
   }
 `;
 
