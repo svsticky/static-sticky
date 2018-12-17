912b8f7193935c4157894162eb033be3aa64b117
Colin de Roos <colinderoos@gmail.com>
2018-12-17 22:58:30 +0100

    create TurnReveal component

--- src/components/activities/Activity.jsx
+++ src/components/activities/Activity.jsx
@@ -1,50 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button, Card, Image } from 'semantic-ui-react';
-import ReactCardFlip from 'react-card-flip';
 import Currency from 'react-currency-formatter';
+import TurnReveal, { Transition, Direction } from '$/components/TurnReveal';
 
 export default class Activity extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isFlipped: false,
+      infoTransition: Transition.hide,
+      infoDirection: Direction.right,
     };
-    this.flipCard = this.flipCard.bind(this);
+    this.animateInfo = this.animateInfo.bind(this);
+    this.revealRef = React.createRef();
   }
 
-  flipCard(e) {
-    e.preventDefault();
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+  animateInfo(event, transition) {
+    event.preventDefault();
+    this.setState({
+      infoTransition: transition,
+      infoDirection: getClosestEdge(event, this.revealRef.current),
+    });
   }
 
   render() {
     const { poster, name } = this.props.activity;
     return (
+      // getBoundingClientRect is undefined on React components, so we need a plain DOM element here.
+      // Putting the eventHandlers on the TurnReveal component also doesn't work for some reason.
       <div
-        onMouseEnter={this.flipCard}
-        onMouseLeave={this.flipCard}
-        onTouchEnd={this.flipCard}
+        onMouseEnter={e => this.animateInfo(e, Transition.show)}
+        onMouseLeave={e => this.animateInfo(e, Transition.hide)}
+        ref={this.revealRef}
       >
-        <ReactCardFlip isFlipped={this.state.isFlipped}>
-          <div key="front">{renderPoster(poster, name)}</div>
-          <div key="back">{renderInfo(this.props.activity)}</div>
-        </ReactCardFlip>
-        <Hidden>{renderPoster(poster, name)}</Hidden>
+        <TurnReveal
+          back={renderPoster(poster, name)}
+          transition={this.state.infoTransition}
+          direction={this.state.infoDirection}
+        >
+          {renderInfo(this.props.activity)}
+        </TurnReveal>
       </div>
     );
   }
 }
 
 const renderPoster = (posterUrl, activityName) => (
-  <Image size="medium" src={posterUrl} alt={'Poster voor ' + activityName} />
+  <Image size="medium" src={posterUrl} alt={'Poster voor ' + activityName}/>
 );
 
 const renderInfo = ({ id, location, name, price }) => (
-  <Card>
-    <Card.Content>
+  <FullSizeCard>
+    <MinHeightContent>
       <Card.Header>{name}</Card.Header>
-    </Card.Content>
+    </MinHeightContent>
     <Card.Content>
       <Card.Description>
         {location && (
@@ -57,7 +66,7 @@ const renderInfo = ({ id, location, name, price }) => (
           <strong>Prijs: </strong>
           <em>
             {price !== 0 ? (
-              <Currency quantity={parseFloat(price)} currency="EUR" />
+              <Currency quantity={parseFloat(price)} currency="EUR"/>
             ) : (
               'Gratis!'
             )}
@@ -65,17 +74,64 @@ const renderInfo = ({ id, location, name, price }) => (
         </p>
       </Card.Description>
     </Card.Content>
-    <Button
-      href={'https://koala.svsticky.nl/activities/' + id}
-      target="_blank"
-      content="Inschrijven"
-      icon="external"
-      labelPosition="right"
-    />
-  </Card>
+    <ButtonWrapper>
+      {/* without a div around the button the height of the label would not be taken into account */}
+      <Button
+        href={'https://koala.svsticky.nl/activities/' + id}
+        target="_blank"
+        content="Inschrijven"
+        icon="external"
+        labelPosition="right"
+      />
+    </ButtonWrapper>
+  </FullSizeCard>
 );
 
-const Hidden = styled.div`
-  z-index: -1;
-  visibility: hidden;
+const MinHeightContent = styled(Card.Content)`
+  &&&& {
+    flex-grow: 0;
+  }
 `;
+
+const FullSizeCard = styled(Card)`
+  &&& {
+    width: 100%;
+  }
+
+  height: 100%;
+  overflow: auto;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+
+  .button {
+    flex: 1;
+  }
+`;
+
+const getClosestEdge = (event, element) => {
+  const { width, height, top, left } = element.getBoundingClientRect();
+  const l = event.pageX - (left + window.pageXOffset);
+  const t = event.pageY - (top + window.pageYOffset);
+
+  const closestHorizontalEdge =
+    t > 0.5 * height
+      ? { edge: Direction.bottom, distance: height - t }
+      : {
+        edge: Direction.top,
+        distance: t,
+      };
+
+  const closestVerticalEdge =
+    l > 0.5 * width
+      ? { edge: Direction.right, distance: width - l }
+      : {
+        edge: Direction.left,
+        distance: l,
+      };
+
+  return closestHorizontalEdge.distance < closestVerticalEdge.distance
+    ? closestHorizontalEdge.edge
+    : closestVerticalEdge.edge;
+};
