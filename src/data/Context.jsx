7082f3458452204c66ce20999599ed5e22542599
Emiel van Rijn <mail@emielvanrijn.nl>
2019-01-25 12:30:24 +0100

    Add updateLastReadNewsPage action to Context

--- src/data/Context.jsx
+++ src/data/Context.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 
-const globalState = React.createContext();
+export const globalState = React.createContext();
 
 class globalStateProvider extends Component {
   state = {
     lastReadNewsPage: 0,
   };
+
+  actions = {
+    updateLastReadNewsPage: page => {
+      this.setState({ lastReadNewsPage: page });
+    },
+  };
+
   render() {
     return (
-      <globalState.Provider value={this.state}>
+      <globalState.Provider
+        value={{
+          state: this.state,
+          actions: this.actions,
+        }}
+      >
         {this.props.children}
       </globalState.Provider>
     );
   }
 }
+
+export default globalStateProvider;
