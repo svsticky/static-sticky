dabc16167a31607c599aabe2bed44a5dde870422
Emiel van Rijn <mail@emielvanrijn.nl>
2019-02-17 19:10:37 +0100

    Rework DryQueries for Index Page by removing unnecessary viewports and tweak necessary

--- src/components/layout/GridDryQueries.jsx
+++ src/components/layout/GridDryQueries.jsx
@@ -39,12 +39,12 @@ const IndexWrapper = styled.div`
   @media ${device.tablet} {
     .container {
       display: grid;
-      grid-template-columns: repeat(2, 1fr);
+      grid-template-columns: 1fr 1fr;
       grid-template-areas:
         'banner banner'
-        'news drinks'
-        'news jobs'
-        'news activity';
+        'news news'
+        'drinks activity'
+        'jobs activity';
       grid-gap: 2em;
     }
     .activity {
@@ -55,47 +55,14 @@ const IndexWrapper = styled.div`
   @media ${device.laptop} {
     .container {
       display: grid;
-      grid-template-columns: repeat(4, 1fr);
+      grid-template-columns: 3fr 1fr;
       grid-template-areas:
-        'banner banner banner banner'
-        'news news news drinks'
-        'news news news jobs'
-        'news news news activity';
-      grid-gap: 2em;
-    }
-    .activity {
-      grid-area: activity;
-      width: auto;
-    }
-  }
-
-  @media ${device.desktop} {
-    .container {
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      grid-template-areas:
-        'banner banner banner banner'
-        'news news news drinks'
-        'news news news jobs'
-        'news news news activity';
-      grid-gap: 2em;
-    }
-    .activity {
-      grid-area: activity;
-      width: auto;
-    }
-  }
-
-  @media ${device.desktopL} {
-    .container {
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      grid-template-areas:
-        'banner banner banner banner'
-        'news news news drinks'
-        'news news news jobs'
-        'news news news activity';
-      grid-gap: 2em;
+        'banner banner'
+        'news drinks'
+        'news jobs'
+        'news activity'
+        'news ...';
+      grid-gap: 3em;
     }
     .activity {
       grid-area: activity;
