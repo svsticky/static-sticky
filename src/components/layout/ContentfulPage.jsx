c88a8497905f7de636186617a63524ab1205628d
Emiel van Rijn <mail@emielvanrijn.nl>
2019-02-19 12:33:09 +0100

    Style dispute index-page

--- src/components/layout/ContentfulPage.jsx
+++ src/components/layout/ContentfulPage.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import Layout from './Layout';
-import styled from 'styled-components';
+import { Card } from 'semantic-ui-react';
 
 export default ({ page, children }) => {
   return (
     <Layout>
-      <PageWrapper>
-        <div>
-          <h2 className="title">{page.title}</h2>
-        </div>
-        <div>{children}</div>
-      </PageWrapper>
+      <h2 className="title">{page.title}</h2>
+      <Card fluid>{children}</Card>
     </Layout>
   );
 };
-
-const PageWrapper = styled.div`
-  .title {
-    border-bottom: 1px solid #ddd;
-    margin-bottom: 10px;
-  }
-`;
