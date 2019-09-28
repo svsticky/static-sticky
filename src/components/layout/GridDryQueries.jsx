dbc6a47445100fb685d05789a0b7fcbdc1f49767
Niels <niels.kwadijk@gmail.com>
2019-09-28 17:38:40 +0200

    Add automatic color change when board changes

--- src/components/layout/GridDryQueries.jsx
+++ src/components/layout/GridDryQueries.jsx
@@ -37,7 +37,7 @@ const IndexWrapper = styled.div`
       grid-gap: 2em;
     }
     .logo {
-      background-color: #c53f78;
+      background-color: ${props => props.color};
       margin: -1rem;
       padding: 0.8rem 0 1rem 0;
       display: flex;
