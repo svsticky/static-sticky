8e8998a92252c39f07dd0a7b54f579f35431bafd
dbrunek <d.j.brunek@students.uu.nl>
2019-11-25 16:54:00 +0100

    Committee and dispute pages formatting

--- src/templates/CommitteeTemplate.jsx
+++ src/templates/CommitteeTemplate.jsx
@@ -3,8 +3,7 @@ import Markdown from 'markdown-to-jsx';
 import styled from 'styled-components';
 import { graphql, Link } from 'gatsby';
 import Layout from '../components/layout/Layout';
-import { Button, Header, Icon, Image } from 'semantic-ui-react';
-import { Composition } from 'atomic-layout';
+import { Button, Header, Icon, Image, Grid, Divider } from 'semantic-ui-react';
 
 const CommitteeView = ({ data: { contentfulCommittee: committee } }) => {
   return (
@@ -17,50 +16,42 @@ const CommitteeView = ({ data: { contentfulCommittee: committee } }) => {
         <Icon name="angle left" />
         Commissies
       </Button>
-      <Composition
-        template={`
-          logo
-          info
-          members
-        `}
-        templateSm={
-          /*I couldn't find a better way to remove the spacing between the logo and members when the info is higher than their combined height, because their rows will get stretched.*/ `
-          logoAboveMembers info
-        `
-        }
-        templateMd={`
-          logo info info members
-        `}
-        gutter="10px"
-        marginTop={'10px' /* spacing for the button */}
-      >
-        {({ Logo, Info, Members, LogoAboveMembers }) => (
-          <>
-            <Logo>
-              <Sticky>{renderLogo(committee)}</Sticky>
-            </Logo>
-            <Info>
-              <Header className="huge">{committee.name}</Header>
-              <Markdown>{committee.description.description}</Markdown>
-            </Info>
-            <LogoAboveMembers>
-              <Sticky>{renderLogo(committee)}</Sticky>
-            </LogoAboveMembers>
-          </>
-        )}
-      </Composition>
+
+      <Grid columns={2} stackable>
+        <Grid.Column width={4}>
+          <Sticky>
+            <Grid.Row>
+              <Image
+                size="medium"
+                src={committee.logo.file.url}
+                alt={`${committee.name} logo`}
+                centered
+              />
+            </Grid.Row>
+            {committee.emailAddress && (
+              <div>
+                <Divider />
+                <Grid.Row>
+                  <h3>Contact</h3>
+                  <p>
+                    <a href={'mailto:' + committee.emailAddress}>
+                      {committee.emailAddress}
+                    </a>
+                    <br />
+                  </p>
+                </Grid.Row>
+              </div>
+            )}
+          </Sticky>
+        </Grid.Column>
+        <Grid.Column width={9}>
+          <Header className="huge">{committee.name}</Header>
+          <Markdown>{committee.description.description}</Markdown>
+        </Grid.Column>
+      </Grid>
     </Layout>
   );
 };
-
-const renderLogo = committee => (
-  <Image
-    src={committee.logo.file.url}
-    alt={`${committee.name} logo`}
-    centered
-    size="medium"
-  />
-);
 
 const Sticky = styled.div`
   top: 60px; // this is the height of the navbar (probably should make that dynamic) + 10 for nice spacing
@@ -77,6 +68,7 @@ export const CommitteeQuery = graphql`
       description {
         description
       }
+      emailAddress
       logo {
         file {
           url
