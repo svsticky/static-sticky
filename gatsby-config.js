85455739d77baafc96ce2e2eee12502b117f5d24
Mstiekema <merijnstiekema@outlook.com>
2020-01-08 14:43:47 +0100

    Added redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
     languages: ['nl', 'en-US'],
+    defaultLocale: 'nl',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
