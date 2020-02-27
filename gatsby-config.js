f61014044829922c1b4f7458db9aff4532d984b3
Mstiekema <merijnstiekema@outlook.com>
2020-02-27 09:41:42 +0100

    Back to the old namespace

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        environment: 'translations',
       },
     },
     {
