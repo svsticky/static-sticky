d078fc81abd5d029fb994a1e68c5fa189a548ee2
Rick van Hoef <rickvhoef@gmail.com>
2020-04-09 16:35:34 +0200

    displayed committees like societies

--- src/static-pages/vereniging/commissies.jsx
+++ src/static-pages/vereniging/commissies.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { graphql, StaticQuery, Link } from 'gatsby';
-import ContentfulPage from '../../components/layout/ContentfulPage';
 import Markdown from 'markdown-to-jsx';
-import { Image, Card, Grid } from 'semantic-ui-react';
-import styled from 'styled-components';
 import { getLanguage, metadata } from '../../data/i18n';
+import { graphql, StaticQuery } from 'gatsby';
+import Layout from '../../components/layout/Layout';
+import Committee from '../../components/Committee';
+import { FlexListContainer } from '../../helpers';
 
 const CommitteeIndexPage = ({ data }) => {
   const language =
@@ -20,39 +20,17 @@ const CommitteeIndexPage = ({ data }) => {
   const page = data.contentfulPage;
 
   return (
-    <ContentfulPage page={page}>
-      <Grid doubling stackable centered columns={3}>
-        {committees.map(committee => (
-          <Grid.Column key={committee.id}>
-            <Card
-              as={Link}
-              fluid
-              to={`/${committee.node_locale}/commissies/${committee.slug}`}
-            >
-              <WhiteBackgroundImage
-                className="white"
-                centered
-                src={committee.logo.file.url}
-                height="300pt"
-              />
-              <Card.Content>
-                <Card.Header>{committee.name}</Card.Header>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-        ))}
-      </Grid>
-      <br />
+    <Layout title={page.title}>
+      <h2>{page.title}</h2>
       <Markdown>{page.content.content}</Markdown>
-    </ContentfulPage>
+      <FlexListContainer>
+        {committees.map(committee => (
+          <Committee committee={committee} key={committee.name} />
+        ))}
+      </FlexListContainer>
+    </Layout>
   );
 };
-
-const WhiteBackgroundImage = styled(Image)`
-  &&& {
-    background: rgba(0, 0, 0, 0);
-  }
-`;
 
 const CommitteeListQuery = graphql`
   query CommitteeListQuery {
