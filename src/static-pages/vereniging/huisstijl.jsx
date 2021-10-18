86b368cb0dbf89aac29308b290b8074f5764cd51
Mstiekema <merijnstiekema@outlook.com>
2021-10-18 14:36:32 +0200

    Added translations

--- src/static-pages/vereniging/huisstijl.jsx
+++ src/static-pages/vereniging/huisstijl.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
 import Layout from '../../components/layout/Layout';
+import { Image, Grid, Container, Dropdown } from 'semantic-ui-react';
 import { getTranslation, getLanguage, metadata } from '../../data/i18n';
-import { Image, Grid, Button, Container, Dropdown } from 'semantic-ui-react';
 
 const columns = ['kleur', 'zwart', 'wit'];
 const rows = ['hoofd', 'logo_compact', 'logo'];
 
-const CreateCell = (row, column) => {
+const CreateCell = (row, column, language) => {
   const white = column.includes('wit');
   const color = white ? 'black' : null;
 
@@ -36,12 +36,12 @@ const CreateCell = (row, column) => {
             <Dropdown.Item
               as="a"
               href={`https://public.svsticky.nl/logos/${row}_gevuld_${column}.svg`}
-              text="Gevulde SVG"
+              text={`${getTranslation(language, 'branding.logo.filled')} SVG`}
             />
             <Dropdown.Item
               as="a"
               href={`https://public.svsticky.nl/logos/${row}_gevuld_${column}.png`}
-              text="Gevulde PNG"
+              text={`${getTranslation(language, 'branding.logo.filled')} PNG`}
             />
           </Dropdown.Menu>
         </Dropdown>
@@ -72,7 +72,7 @@ const Huisstijl = props => {
   rows.forEach(row => {
     let new_columns = [];
     columns.forEach(column => {
-      new_columns.push(CreateCell(row, column));
+      new_columns.push(CreateCell(row, column, language));
     });
     all_rows.push(new_columns);
   });
@@ -80,17 +80,8 @@ const Huisstijl = props => {
   return (
     <Layout title={title}>
       <h1>{title}</h1>
-      <h2>Logo's</h2>
-      <p>
-        Het Sticky logo heeft drie verschillende vormen; Het hoofd, de compactie
-        versie en het volledige logo. Het hoofd wordt voornamelijk gebruikt op
-        Sticky merchandise of handtekeningen van mails. De compacte versie is
-        ook terug te vinden op bepaalde merchandise items. Verder wordt dit logo
-        voornamelijk gebruikt bij het maken van posters voor activiteiten van de
-        vereniging. Als laatste wordt het volledige logo voonramelijk voor breed
-        en externe doeleinden gebruikt. Hierbij kan je denken aan promotie
-        richting bedrijven en universiteit(en) toe.
-      </p>
+      <h2>{getTranslation(language, 'branding.logo.title')}</h2>
+      <p>{getTranslation(language, 'branding.logo.text')}</p>
       <br />
       <Grid divided>
         {all_rows.map(row => {
@@ -103,17 +94,14 @@ const Huisstijl = props => {
           );
         })}
       </Grid>
-      <h2>Kleuren</h2>
+      <h2>{getTranslation(language, 'branding.colors.title')}</h2>
       <p>
-        Elk bestuur bij Sticky heeft zijn eigen bestuurskleur.
-        <i> Dit jaar is het {props.data.contentfulBoard.color}. </i>
-        Sticky heeft oranje als verenigingskleur, maar deze wordt niet altijd
-        gebruikt. Voor het gebruik van kleur bij promotie van Sticky wordt
-        aangeraden om niet telkens met 1 kleur te werken, maar meerderen die
-        goed op elkaar aansluiten. Hiervoor zijn websites als{' '}
-        <a href="https://coolors.co">Coolors</a> en{' '}
+        {getTranslation(language, 'branding.colors.text', [
+          props.data.contentfulBoard.color,
+        ])}{' '}
+        <a href="https://coolors.co">Coolors</a> &{' '}
         <a href="https://color.adobe.com">Adobe Color Wheel</a>.
-        <h3>Kleuroverzicht</h3>
+        <h3>{getTranslation(language, 'branding.colors.chart')}</h3>
         <table style={tableStyle(props.data.contentfulBoard.color)}>
           <tbody>
             <tr>
@@ -131,53 +119,58 @@ const Huisstijl = props => {
           </tbody>
         </table>
       </p>
-      <h2>Lettertypes</h2>
+      <h2>{getTranslation(language, 'branding.fonts.title')}</h2>
+      <p>{getTranslation(language, 'branding.fonts.text')}</p>
+      <h2>{getTranslation(language, 'branding.posters.title')}</h2>
       <p>
-        In posters wordt er aangeraden om maximaal 1 of 2 fonts te gebruiken. Zo
-        wordt de poster niet te vol. De lettertype families 'serif' en
-        'sans-serif' worden voornamelijk gebruikt, maar er is altijd vrijheid om
-        een andere familie te gebruiken.
-      </p>
-      <h2>Posters</h2>
-      <p>
-        De posters bij Sticky hebben een aantal vaste requirements staan. Deze
-        zijn er om de kwaliteit en zichtbaarheid van de posters te verbeteren.
-        Deze luiden;
+        {getTranslation(language, 'branding.posters.text')}
         <ul>
           <li>
-            Formaat: <b>A3</b>
+            {getTranslation(language, 'branding.posters.requirements.format')}:{' '}
+            <b>A3</b>
           </li>
           <li>
-            Resolutie: <b>300dpi</b>
+            {getTranslation(
+              language,
+              'branding.posters.requirements.resolution'
+            )}
+            : <b>300dpi</b>
           </li>
           <li>
-            Oriëntatie: <b>Portret</b>
+            {getTranslation(
+              language,
+              'branding.posters.requirements.orientation'
+            )}
+            :{' '}
+            <b>
+              {getTranslation(
+                language,
+                'branding.posters.requirements.portrait'
+              )}
+            </b>
           </li>
           <li>
-            Bestandsformaat: <b>PDF</b>
+            {getTranslation(
+              language,
+              'branding.posters.requirements.file_format'
+            )}
+            : <b>PDF</b>
           </li>
           <li>
-            Taal: <b>Engels</b>
+            {getTranslation(language, 'branding.posters.requirements.language')}
+            :{' '}
+            <b>
+              {getTranslation(
+                language,
+                'branding.posters.requirements.english'
+              )}
+            </b>
           </li>
         </ul>
-        <h3>Activiteitenposters</h3>
-        <p>
-          De titel van de activiteit, datum, locatie, commissie logo en het
-          Sticky logo moeten ook altijd te vinden zijn op de posters. Voor de
-          datum zijn er geen harde lijnen, maar wordt er geadviseerd de
-          DD-MM(m), DD-MM-YY of DD-MM-YYYY notatie te gebruiken. Het enige wat
-          wordt afgeraden is de Amerikaanse MM-DD notatie, gezien dit verwarring
-          kan brengen met in welke maand de activiteit plaats vind.
-        </p>
-        <h3>Externposters</h3>
-        <p>
-          De posters voor extern of onderwijs gerelateerde activiteiten hebben
-          soms wat extra vereisten. Zo moet in plaats van het commissie logo het
-          logo van een bedrijf komen te staan. Verder wordt het gebruik van het
-          volledige logo hier ook aangeraden. De stijl van de poster wordt vaker
-          serieuzer en proffessioneler opgesteld om de aard van de activiteit te
-          laten zien.
-        </p>
+        <h3>{getTranslation(language, 'branding.posters.activities_title')}</h3>
+        <p>{getTranslation(language, 'branding.posters.activities')}</p>
+        <h3>{getTranslation(language, 'branding.posters.external_title')}</h3>
+        <p>{getTranslation(language, 'branding.posters.external')}</p>
       </p>
     </Layout>
   );
