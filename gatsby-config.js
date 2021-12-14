05cdfd2eb22cfbdbdc7ce3958992e7ef5100eee0
Rick van Hoef <rickvhoef@gmail.com>
2021-12-14 16:31:05 +0100

    readd favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,14 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/images/favicon.svg`,
+        icon_options: {
+          purpose: `maskable`,
+        },
+      },
+    },
   ],
 };
