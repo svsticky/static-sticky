70f2c541bcc97fe7c51e2c704bb71e19c41623da
Job Vonk <sticcpistt@icloud.com>
2023-05-08 16:11:07 +0200

    Add short text and markdown formatting to main partner card on partner page (#365)
    * Changed class to className in Footer component
    
    * Added main partner to partners page
    
    * Added main partner to home page and nav
    
    * Main partner pages cleanup
    
    * removed navbar promo and banner styling
    
    * Added markdown rendering for main partner
    
    * Added a short text for the main partner
    
    ---------
    
    Co-authored-by: Jelte-Akker <jelte2410@gmail.com>

--- src/static-pages/carriere/partners.jsx
+++ src/static-pages/carriere/partners.jsx
@@ -63,6 +63,9 @@ const PartnerListQuery = graphql`
           isMainPartner
           node_locale
           name
+          shortText {
+            shortText
+          }
           slug
           website
           description {
