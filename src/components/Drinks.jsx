f811a9587e316d5724a3aacf4266c29202bf439e
Sander <sander@shageman.nl>
2023-09-30 20:00:47 +0200

    update drinks to richer content

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react';
 import { graphql, StaticQuery } from 'gatsby';
-import { getTranslation, getLanguage, metadata } from '../data/i18n';
+import { getLanguage, metadata } from '../data/i18n';
+import Markdown from 'markdown-to-jsx';
 
 class Drinks extends React.Component {
   renderDescription = (drinks, lg) => {
     return (
-      <>
-        {drinks.description}
-        <b>
-          {getTranslation(lg, 'drinks.when', [
-            drinks.day,
-            drinks.time,
-            drinks.location,
-          ])}
-        </b>
-      </>
+      <Markdown>
+        {drinks.descriptionlong.descriptionlong}
+      </Markdown>
     );
   };
 
@@ -42,12 +36,11 @@ const drinksQuery = graphql`
   query drinksQuery {
     allContentfulDrinks {
       nodes {
-        day
         node_locale
-        time
-        location
         title
-        description
+        descriptionlong {
+          descriptionlong
+        }
       }
     }
   }
